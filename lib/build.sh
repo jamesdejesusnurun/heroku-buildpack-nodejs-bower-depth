1684a7e2e7bb2bf16731993562b3a489a73321b4
James De Jesus <xjamesx@SDF1.hsd1.ca.comcast.net>
2015-03-31 20:31:27 -0700

    adding compass function

--- lib/build.sh
+++ lib/build.sh
@@ -104,6 +104,30 @@ show_current_state() {
   info "NODE_MODULES_CACHE=$NODE_MODULES_CACHE"
 }
 
+install_compass() {
+  # install compass
+  status "Installing Compass"
+  export GEM_HOME=$build_dir/.gem/ruby/1.9.1
+  PATH="$GEM_HOME/bin:$PATH"
+  if test -d $cache_dir/ruby/.gem; then
+    status "Restoring ruby gems directory from cache"
+    cp -r $cache_dir/ruby/.gem $build_dir
+    HOME=$build_dir gem update compass --user-install --no-rdoc --no-ri
+  else
+    HOME=$build_dir gem install compass --user-install --no-rdoc --no-ri
+  fi
+
+  # cache ruby gems compass
+  rm -rf $cache_dir/ruby
+  mkdir -p $cache_dir/ruby
+
+  # If app has a gems directory, cache it.
+  if test -d $build_dir/.gem; then
+    status "Caching ruby gems directory for future builds"
+    cp -r $build_dir/.gem $cache_dir/ruby
+  fi
+}
+
 install_node() {
   local node_engine=$1
 
